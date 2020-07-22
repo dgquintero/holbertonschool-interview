d337f3454d5ec04f1fa4c01b80ca5ef7451e94d7
Daniel Quintero <dgquintero02@hotmail.com>
2020-07-22 16:24:11 -0500

    pep8 style fixed

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,10 +2,11 @@
 
 
 def canUnlockAll(boxes):
+    """ method that determines if all the boxes can be opened """
     open_boxes = [0]
     for i in open_boxes:
         for box in boxes[i]:
-            if box not in open_boxes and box <len(boxes):
+            if box not in open_boxes and box < len(boxes):
                 open_boxes.append(box)
     if (len(boxes) == len(open_boxes)):
         return True
