6ff177fea4952cc1b0de1bfbc8be20220b369396
Daniel Quintero <dgquintero02@hotmail.com>
2020-07-22 16:20:43 -0500

    adding answer

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    open_boxes = [0]
+    for i in open_boxes:
+        for box in boxes[i]:
+            if box not in open_boxes and box <len(boxes):
+                open_boxes.append(box)
+    if (len(boxes) == len(open_boxes)):
+        return True
+    else:
+        return False
