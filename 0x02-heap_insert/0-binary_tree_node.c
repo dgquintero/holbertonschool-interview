48be29b52648b96c7eb87953cdb3b70f02c66fe7
Daniel Quintero <dgquintero02@hotmail.com>
2020-08-04 11:28:05 -0500

    fixing binary tree node

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -4,7 +4,7 @@
 
 
 /**
- * binary_tree_t - function that creates a binary tree node
+ * binary_tree_node - function that creates a binary tree node
  *
  * @parent: Pointer to the parent node
  * @value: is the value to put in the new node
@@ -15,6 +15,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
     binary_tree_t *new_node;
 
     new_node = malloc(sizeof(binary_tree_t));
+    if (new_node == NULL)
+        return (NULL);
     new_node->parent = parent;
     new_node->n = value;
     new_node->left = NULL;
