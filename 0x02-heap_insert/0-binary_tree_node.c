cf537bafeb01e6b40cca5310b505ad73524d8420
Daniel Quintero <dgquintero02@hotmail.com>
2020-08-04 10:52:52 -0500

    adding header and 0 file

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_t - function that creates a binary tree node
+ *
+ * @parent: Pointer to the parent node
+ * @value: is the value to put in the new node
+ * Return: return a pointer to the new node or Null on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    *new_node;
+    
+}
