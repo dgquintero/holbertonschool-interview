828ccd900bb4b402d661e266f6342c02e979e38d
Daniel Quintero <dgquintero02@hotmail.com>
2020-08-04 16:15:07 -0500

    changes on betty style

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -5,18 +5,16 @@
 
 /**
  * binary_tree_size - function that return the size of a tree
- * @tree: tree to check
- * Return: size of tree
+ * @tree: tree to 
+ * Return: tree size
  */
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size = 0, r = 0, l = 0;
+	size_t size, r, l = 0;
 
 	if (tree == NULL)
-	{
 		return (0);
-	}
 	else
 	{
 		l = binary_tree_size(tree->left);
